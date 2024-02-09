3c64d4c9035c566bbb9c8ae0293fe898066bc1f0
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2024-02-09 17:34:18 +0530

    fix: shifting to a new project due to security

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,12 @@
 import firebase from "firebase";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
-//   authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
-//   projectId: "disneyplus-clone-a33d5",
-//   storageBucket: "disneyplus-clone-a33d5.appspot.com",
-//   messagingSenderId: "37918794208",
-//   appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
-//   measurementId: "G-DRVLJKWRWG",
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDky-xDNyY_uAuAS7wBxVhhBH2ipVwlsLo",
-  authDomain: "disney-plus-feature.firebaseapp.com",
-  projectId: "disney-plus-feature",
-  storageBucket: "disney-plus-feature.appspot.com",
-  messagingSenderId: "396674138536",
-  appId: "1:396674138536:web:fe1196fa953c61325d1d9d"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
